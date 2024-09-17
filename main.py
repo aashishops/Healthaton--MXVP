3196d6ed9c53b9e6597330d72872d098eacec11d
Sunetra Banerjee <sb7739@srmist.edu.in>
2024-09-17 19:59:24 +0530

    used fastapi

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
+from PIL import Image
+import io
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="templates", html=True), name="templates")
+@app.post("/upload")
+async def upload_image(file: UploadFile = File(...)):
+    # Open the uploaded image file
+    image = Image.open(file.file)
+    
+    # Process the image (if needed)
+    # For now, we just return the uploaded image
+    processed_image = image
 
+    # Save the image to an in-memory buffer
+    img_io = io.BytesIO()
+    processed_image.save(img_io, 'JPEG')
+    img_io.seek(0)
 
+    # Return the image as a StreamingResponse
+    return StreamingResponse(img_io, media_type="image/jpeg")
+
+# To run the app, use:
+# uvicorn filename:app --reload
